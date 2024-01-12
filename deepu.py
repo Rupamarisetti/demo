7ccf77d41e36fa820a791b2977b9e1f269363f36
Rupamarisetti <147685234+Rupamarisetti@users.noreply.github.com>
2024-01-12 10:36:18 +0530

    Create deepu.py

---
+++ deepu.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('DEEPTHI SUNINAA')
+st.write('DEEPU')
+st.image('https://w0.peakpx.com/wallpaper/608/282/HD-wallpaper-deepthi-sunaina-flash-graphy-lip.jpg',caption='DEEPTHI')
