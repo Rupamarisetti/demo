2e806db09450d47a124d7497d41f4a5ee3a09fb5
Rupamarisetti <147685234+Rupamarisetti@users.noreply.github.com>
2024-01-12 10:40:32 +0530

    Create deepu2.py

---
+++ deepu2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header('DEEPTHI SUNINAA')
+st.write('DEEPU')
+
+# Embedding YouTube video
+video_url = 'https://www.youtube.com/watch?v=PbPODDfgQMA'
+st.video(video_url, caption='DEEPTHI')
